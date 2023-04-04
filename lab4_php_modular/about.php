39326dae16a5266c9953a7c2e0ff0a89da8ae274
dwitaaeni <92616861+dwitaaeni@users.noreply.github.com>
2023-04-04 13:17:35 +0700

    Add files via upload

---
+++ lab4_php_modular/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>About</h2>
+    <div class="box">
+        <p>Hai saya D.Wita Aeni mengucapkan terimakasih.</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
